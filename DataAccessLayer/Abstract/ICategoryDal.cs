1b5e5a28ae7331a5a733948c9879d189b500bfa7
Göktuğ OKTAY <ggoktugoktay@gmail.com>
2022-03-14 17:55:03 +0300

    first commit

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //İsimlendirme
+    //------------
+    //Interface olduğu için Başına I koyulur.
+    //İlgili alan adı olan Category yazılır.
+    //Data Access Layer içinde olduğu için sonun Dal kısaltması eklenir.
+    public interface ICategoryDal : IGenericDal<Category>
+    {
+    }
+}
